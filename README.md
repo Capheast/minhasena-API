c44099519ba2e476fd93f77193fc6649fd9cffb2
Vitor Juca <vitor.juca222@gmail.com>
2019-06-08 20:30:32 -0300

    Added number generator endpoint

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # minhasena-API
+
+## Routes
+
+
+> GET /generate
+> 
+>response : {"numbers":[25,38,29,32,44,16,14]}
+
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/generate', function(req, res, next) {
+
+  let numbers = [];
+
+  while (numbers.length <= 6){
+    let generatedNumber = Math.ceil(Math.random() * 60);
+    if (!numbers.includes(generatedNumber)) {
+      numbers.push(generatedNumber);
+    }
+  }
+
+  res.json({numbers});
 });
 
 module.exports = router;
