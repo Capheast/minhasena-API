b326cbc86ff652f95d56e10aa3679f0e8f466075
josemiguelq <jose.miguelq95@gmail.com>
2019-06-08 23:02:36 -0300

    sorteio

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/generate', function(req, res, next) {
+router.get('/generate', function (req, res, next) {
 
-  let numbers = []
+    let numbers = []
 
-  while (numbers.length <= 6){
-    let generatedNumber = Math.ceil(Math.random() * 60);
-    if (!numbers.includes(generatedNumber)) {
-      numbers.push(generatedNumber)
+    while (numbers.length <= 6) {
+        let generatedNumber = Math.ceil(Math.random() * 60);
+        if (!numbers.includes(generatedNumber)) {
+            numbers.push(generatedNumber)
+        }
     }
-  }
 
-  numbers.sort((current, next) => current - next)
+    numbers.sort((current, next) => current - next)
 
-  res.json({numbers})
+    res.json({numbers})
 });
+
+router.get('/setup', (req, res, next) => {
+    let setup = {}
+    setup.sorteios = {
+        "ttilte": "Escolha o sorteio",
+        "items": [
+            {
+                "title": "Mega Sena",
+                "subTitle": "Acerte 6 números e gannhe uma bolada",
+                "text": "Sorteio atual de R$180 milhões"
+            },
+            {
+                "title": "Mega Sena",
+                "subTitle": "Acerte 6 números e gannhe uma bolada",
+                "text": "Sorteio atual de R$180 milhões"
+            },
+        ]
+    }
+
+    setup.highlight = [
+        {
+            "title": "Mega Sena",
+            "subTitle": "Acerte 6 números e gannhe uma bolada",
+            "text": "Sorteio atual de R$180 milhões"
+        },
+    ]
+    res.json(setup)
+})
 
 module.exports = router;
