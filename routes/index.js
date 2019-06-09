e2e75ae4f2270934d59f370b4274c10f5ba619f9
Vitor Juca <vitor.juca222@gmail.com>
2019-06-08 21:22:08 -0300

    Added sort

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,18 @@ var router = express.Router();
 /* GET home page. */
 router.get('/generate', function(req, res, next) {
 
-  let numbers = [];
+  let numbers = []
 
   while (numbers.length <= 6){
     let generatedNumber = Math.ceil(Math.random() * 60);
     if (!numbers.includes(generatedNumber)) {
-      numbers.push(generatedNumber);
+      numbers.push(generatedNumber)
     }
   }
 
-  res.json({numbers});
+  numbers.sort((current, next) => current - next)
+
+  res.json({numbers})
 });
 
 module.exports = router;
