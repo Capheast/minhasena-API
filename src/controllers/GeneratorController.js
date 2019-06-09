04922e578d774c7a971837965e9b288b45d41e84
Vitor Juca <vitor.juca222@gmail.com>
2019-06-09 15:29:44 -0300

    wip

--- src/controllers/GeneratorController.js
+++ src/controllers/GeneratorController.js
@@ -1,14 +1,23 @@
 const numberGeneratorService = require('../services/numberGeneratorService');
+var Number = require('../models/Number');
 
-exports.get = (req,res,next) => {
+exports.get = (req, res, next) => {
     let result = numberGeneratorService.generate();
+    result.forEach((item) => {
+        if (!!Number.find({ value: item })) {
+            let number = new Number({ value: item });
+            number.save((err) => {
+                if (err) console.log(err)
+            });
+        }
+    })
     res.json(result);
 }
 
-exports.post = (req,res,next) => {
-    res.json({message: 'POST'})
+exports.post = async (req, res, next) => {
+    res.json({ message: 'POST' })
 }
 
-exports.put = (req,res,next) => {
-    res.json({message: 'PUT'})
+exports.put = (req, res, next) => {
+    res.json({ message: 'PUT' })
 }
