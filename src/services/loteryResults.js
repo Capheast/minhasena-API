0571ff7836060ff9e17e31a956cd37fd68e546f2
josemiguelq <jose.miguelq95@gmail.com>
2019-07-01 22:13:58 -0300

    axios para buscar resultados

---
+++ src/services/loteryResults.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const getResults = (lotteryType) => {
+    axios.get(`https://www.lotodicas.com.br/api//${lotteryType}`)
+    .then(response => {
+        return response.data
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    return {}
+}
