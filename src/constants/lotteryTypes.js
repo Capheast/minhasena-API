682891b572ba7560cec2d105bf7048acb2aabe47
josemiguelq <jose.miguelq95@gmail.com>
2019-08-05 23:11:01 -0300

    title subtitle

--- src/constants/lotteryTypes.js
+++ src/constants/lotteryTypes.js
@@ -1,7 +1,7 @@
-export const lotofacil = { title: 'Lotofácil', color: '#AB1D88', constant: 'lotofacil'}
-export const megaSena = { title: 'Mega Sena', color: '#00B758', constant: 'mega-sena'}
-export const quina = { title: 'Quina', color: '#42338B', constant: 'quina'}
-export const duplaSena = { title: 'Dupla Sena', color: '#897987', constant: 'dupla-sena'}
-export const lotomania = { title: 'Lotomania', color: '#f78100', constant: 'lotomania'}
-export const timenania = { title: 'Timemania', color: '#f78100', constant: 'timemania'}
-export const diaDeSorte = { title: 'Dia de Sorte', color: '#f78100', constant: 'dia-de-sorte'}
+export const lotofacil = { title: 'Lotofácil', color: '#AB1D88', constant: 'lotofacil', subtitle: 'Sorteio atual de R$420 mil' }
+export const megaSena = { title: 'Mega Sena', color: '#00B758', constant: 'mega-sena', subtitle: 'Sorteio atual de R$180 milhões' }
+export const quina = { title: 'Quina', color: '#42338B', constant: 'quina', subtitle: 'Sorteio atual de R$80 mil' }
+export const duplaSena = { title: 'Dupla Sena', color: '#897987', constant: 'dupla-sena', subtitle: '' }
+export const lotomania = { title: 'Lotomania', color: '#f78100', constant: 'lotomania', subtitle: 'Sorteio atual de R$1.6 milhões' }
+export const timenania = { title: 'Timemania', color: '#f78100', constant: 'timemania', subtitle: '' }
+export const diaDeSorte = { title: 'Dia de Sorte', color: '#f78100', constant: 'dia-de-sorte', subtitle: '' }
