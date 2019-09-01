7a765307775e2397fb846b110d1917a8b5ad6946
josemiguelq <jose.miguelq95@gmail.com>
2019-09-01 20:41:00 -0300

    ajustes de detalhes

--- src/constants/lotteryTypes.js
+++ src/constants/lotteryTypes.js
@@ -4,38 +4,44 @@ export const lotofacil = {
     constant: 'lotofacil',
     howToPlay: 'A Mega-Sena paga milhões para o acertador dos 6 números sorteados. Ainda é possível ganhar prêmios ao acertar 4 ou 5 números dentre os 60 disponíveis no volante de apostas. Para realizar o sonho de ser o próximo milionário, você deve marcar de 6 a 15 números do volante, podendo deixar que o sistema escolha os números para você (Surpresinha) e/ou concorrer com a mesma aposta por 2, 4 ou 8 concursos consecutivos (Teimosinha).',
     configs: {
-        min: 15,
-        max: 18,
-        tries: [11, 12, 13, 14, 15]
+        min: 1,
+        max: 25,
+        tries: [11, 15]
     }
 
 }
 export const megaSena = {
     title: 'Mega Sena',
     color: '#209869',
+    howToPlay: "A Lotofácil é, como o próprio nome diz, fácil de apostar e principalmente de ganhar. Você marca" +
+        " entre 15 a 18 números, dentre os 25 disponíveis no volante, e fatura o prêmio se acertar 11, 12, 13, 14 ou 15 números. Pode ainda deixar que o sistema escolha os números para você por meio da Surpresinha, ou concorrer com a mesma aposta por 3, 6, 9 ou 12 concursos consecutivos através da Teimosinha.",
     constant: 'mega-sena',
     configs: {
-        min: 6,
-        max: 15,
-        tries: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        min: 1,
+        max: 60,
+        tries: [6, 15]
     }
 }
 export const quina = {
     title: 'Quina',
     color: '#260085',
+    howToPlay: "Concorra a prêmios grandiosos com a Quina: basta marcar de 5 a 15 números dentre os 80 disponíveis no volante e torcer. Caso prefira o sistema pode escolher os números para você através da Surpresinha." +
+        +"Ganham prêmios os acertadores de 2, 3, 4 ou 5 números. Você ainda pode concorrer com a mesma aposta por 3, 6, 12, 18 ou 24 concursos consecutivos com a Teimosinha.",
     constant: 'quina',
     configs: {
-        min: 5,
-        max: 15,
-        tries: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        min: 1,
+        max: 80,
+        tries: [5, 15]
     }
 }
 export const duplaSena = {
-    title: 'Dupla Sena', color: '#a61324', constant: 'dupla-sena', subtitle: '',
+    title: 'Dupla Sena',
+    color: '#a61324',
+    constant: 'dupla-sena',
     configs: {
-        min: 6,
-        max: 15,
-        tries: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        min: 1,
+        max: 50,
+        tries: [6, 15]
     }
 }
 export const lotomania = {
@@ -44,16 +50,17 @@ export const lotomania = {
     constant: 'lotomania',
     configs: {
         min: 0,
-        max: 50,
-        tries: [20, 19, 18, 17, 16, 15, 0]
+        max: 99,
+        tries: [15, 20]
     }
 }
 export const timenania = {
-    title: 'Timemania', color: '#00ff48', constant: 'timemania',
+    title: 'Timemania',
+    color: '#00ff48',
+    constant: 'timemania',
     configs: {
         min: 3,
         max: 10,
-        tries: [3, 4, 5, 6, 7]
     }
 }
 export const diaDeSorte = {
@@ -63,6 +70,5 @@ export const diaDeSorte = {
     configs: {
         min: 7,
         max: 15,
-        tries: [7, 8, 9, 10, 11, 12, 13, 14, 15]
     }
 }
